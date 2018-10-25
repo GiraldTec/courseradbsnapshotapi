f1e4a6c6e9a38d4197aaf931bdd67081d4661dfe
Carlos Giraldo <cacu2ggg@gmail.com>
2018-10-25 16:52:37 +0200

    making use of the empty tables script

--- automatic/automatic_requester.py
+++ automatic/automatic_requester.py
@@ -44,10 +44,10 @@ TABLES_REQUEST, CLICKSTREAM_REQUEST, RANGE_LAST = tools.issue_requests(CONFIG_JS
 
 TABLES_FILE, CLICKSTREAM_FILES, CONFIG_JSON = tools.download_requests(TABLES_REQUEST, CLICKSTREAM_REQUEST, CONFIG_JSON)
 
-#CONFIG_JSON = tools.update_database_tables(TABLES_FILE, CONFIG_JSON)
-#CONFIG_JSON = tools.update_database_clickstream(CLICKSTREAM_FILES, CONFIG_JSON)
+CONFIG_JSON = tools.update_database_tables(TABLES_FILE, CONFIG_JSON)
+CONFIG_JSON = tools.update_database_clickstream(CLICKSTREAM_FILES, CONFIG_JSON)
 
-#tools.update_jsonfile(RANGE_LAST,sys.argv[1],CONFIG_JSON)
+tools.update_jsonfile(RANGE_LAST,sys.argv[1],CONFIG_JSON)
 
 # to run the script that refreshes the materialized views
 # sudo -u www-data /srv/apps/coursera/venv/bin/python /srv/apps/coursera/code/manage.py refreshviews
